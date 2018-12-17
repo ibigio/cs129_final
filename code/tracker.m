43a052d86a76f1d7c4a74688eb468849d5a38c51
Young <younggzzheng@gmail.com>
2018-12-17 14:35:56 -0500

    initial commit

--- code/tracker.m
+++
@@ -1,8 +0,0 @@
-clear;        
-patch_size = 20;
-homo_tracker = imread('marker2.jpg');
-picture = imread('IMG_4684.jpg');
-
-
-[X1 Y1 X2 Y2] = define_correspondence(homo_tracker, picture, patch_size);
-show_correspondence(homo_tracker, picture, X1, Y1, X2, Y2);
