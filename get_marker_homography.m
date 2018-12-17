849e12b69b1365b8cb259d42d24ff51b61d5766a
Young <younggzzheng@gmail.com>
2018-12-17 18:20:04 -0500

    it works now hell yeah
    homography_testing has two images that are useful for testing. instead of outputting homography, outputs transform

--- get_marker_homography.m
+++ get_marker_homography.m
@@ -1,4 +1,4 @@
-function [ H ] = get_marker_homography(input_img,marker)
+function [ transform ] = get_marker_homography(input_img,marker)
 %GET_MARKER_HOMOGRAPHY Finds homography between input_img and marker
 %   
 %   Inputs:
@@ -6,8 +6,30 @@ function [ H ] = get_marker_homography(input_img,marker)
 %       - marker :      marker to find in input_img
 %
 %   Output:
-%       - H :           homography between input_img and marker
+%       - transform :           the projective transform that takes marker to
+%       input_img
 
-H = ones(3);
+marker = rgb2gray(marker);
+input_img = rgb2gray(input_img);
+
+ptsOriginal  = detectSURFFeatures(input_img);
+ptsDistorted = detectSURFFeatures(marker);
+[featuresOriginal,validPtsOriginal] = ...
+    extractFeatures(input_img,ptsOriginal);
+[featuresDistorted,validPtsDistorted] = ...
+    extractFeatures(marker,ptsDistorted);
+
+index_pairs = matchFeatures(featuresOriginal,featuresDistorted);
+matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
+matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
+
+[transform,~,~] = ...
+    estimateGeometricTransform(matchedPtsDistorted,matchedPtsOriginal,...
+    'projective');
+
+outputView = imref2d(size(input_img));
+Ir = imwarp(marker,transform,'OutputView',outputView);
+figure; imshow(Ir); 
+title('Recovered image');
 end
 
