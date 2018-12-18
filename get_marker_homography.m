76ad07fa70768110582b3708ed1ce6f519ae439c
Young <younggzzheng@gmail.com>
2018-12-18 00:04:41 -0500

    more mask stuff

--- get_marker_homography.m
+++ get_marker_homography.m
@@ -1,7 +1,7 @@
 function [ transform ] = get_marker_homography(input_img,marker)
 %GET_MARKER_HOMOGRAPHY Finds homography between input_img and marker
 %   
-%   Inputs:
+%   Inputs: 
 %       - input_img :   input image with a marker somewhere in the frame
 %       - marker :      marker to find in input_img
 %
--- homography_testing.m
+++ homography_testing.m
@@ -1,8 +1,21 @@
 function homography_testing
 % [cropped_marker, cropped_photo, photo_mask] = get_cropped_images(imread('marker2.jpg'),imread('testing/media/photo9.jpg'));
-image = imread('testing/media/photo9.jpg');
-marker = imread('marker2.jpg');
-transform = get_marker_homography(image,marker);
+% image = im2double(imread('testing/media/photo9.jpg'));
+% figure(1)
+% imshow(photo_mask);
 
-composite_images(image, marker, transform.T);
+
+image = im2double(imread('testing/media/photo9.jpg'));
+marker = im2double(imread('marker2.jpg'));
+transform = get_marker_homography(image,marker);
+% imshow(composite_images(image, marker, transform.T));
+
+mask = zeros(size(image));  
+one = ones(size(marker));
+comp = logical(composite_images(mask, one, transform.T));
+stats = regionprops(comp(:,:,1),'centroid');
+offset = stats.Centroid
+
+
+
 % get_marker_homography(cropped_photo,cropped_marker)
