52e5b527a852e9acab8d6f08eb99865d07f06029
Young <younggzzheng@gmail.com>
2018-12-17 21:50:15 -0500

    asdf

--- homography_testing.m
+++ homography_testing.m
@@ -1,4 +1,8 @@
 function homography_testing
-[cropped_marker, cropped_photo, photo_mask] = get_cropped_images(imread('marker2.jpg'),imread('testing/media/photo9.jpg'));
-get_marker_homography(imread('testing/media/photo9.jpg'),imread('marker2.jpg'))
+% [cropped_marker, cropped_photo, photo_mask] = get_cropped_images(imread('marker2.jpg'),imread('testing/media/photo9.jpg'));
+image = imread('testing/media/photo9.jpg');
+marker = imread('marker2.jpg');
+transform = get_marker_homography(image,marker);
+
+composite_images(image, marker, transform.T);
 % get_marker_homography(cropped_photo,cropped_marker)
